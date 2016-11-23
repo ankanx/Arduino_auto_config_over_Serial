6e83a54b170a9c58131a4fb73282c972aeeebf71
Andreas Fransson <ankanx@gmail.com>
2016-11-23 14:22:16 +0100

    Updated port serching script.

--- search_ports.py
+++ search_ports.py
@@ -6,7 +6,21 @@ import serial.tools.list_ports
 #show device iformation
 dev = usb.core.find(idVendor=0x2341, idProduct=0x0043)
 if dev is None:
-    raise ValueError('Our device is not connected')
+    #raise ValueError('Our device is not connected')
+    print ""
+    print "Target Device is not connected!"
+    print ""
+    print "Listing connected devices..."
+    print "*****************************************************************"
+    print usb.core.show_devices()
+    devices = usb.core.find(find_all=True)
+    for dev in devices:
+        print ""
+        print "------> " + dev.product
+        print ""
+        print dev._get_full_descriptor_str()
+    print "*****************************************************************"
+    
 else:
     print "Connected a device.."
     print dev
@@ -21,7 +35,17 @@ else:
     print dev.__iter__
     print dev._langids
 
-#List actual ports    
-ports = list(serial.tools.list_ports.comports())
-for p in ports:
-    print p
+#List Active ports
+print ""
+print "Avaveble ports:"    
+print serial.tools.list_ports.main()
+print serial.tools.list_ports.comports()
+print "Linux COM Ports Active:"
+print serial.tools.list_ports_linux.comports()
+print ""
+
+if serial.tools.list_ports.comports() != []:
+    print "Found ports!"
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        print p
