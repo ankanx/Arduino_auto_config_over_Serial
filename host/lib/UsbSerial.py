821a4676c10d24a3c25d7237e1f70fcb4a0dafa1
Andreas Fransson <ankanx@gmail.com>
2016-11-27 20:53:29 +0100

    Updated and added a reading thread to read confirmation data.

--- host/lib/UsbSerial.py
+++ host/lib/UsbSerial.py
@@ -1,6 +1,7 @@
 import sys
 import serial
-
+import threading
+import time
 
 class USBclient():
     def connect(self,sPort,baud):
@@ -10,6 +11,9 @@ class USBclient():
             global serialConn
             serialConn = self.serialConn
             self.connectionMade()
+            reading_service = reader()
+            reading_service.start()
+            reading_service.setName("Reading Thread")
         except:
             print "Connection failed!"
             print "Unexpected error:", sys.exc_info()[0]
@@ -28,5 +32,24 @@ class USBclient():
     def send(self,data):
         print 'Sending ', repr(data), ' to device.'
         serialConn.write(data)
+        # .encode()?
+        #serialConn.flush()
 
 
+class reader(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+ 
+    
+        # Main
+        def run(self):
+            print "Starting Reader"
+            print ""
+            # Continious port serch
+            while True:
+                time.sleep(1)
+                print threading.currentThread().getName(), "Heartbeat"
+                if serialConn.in_waiting > 0:
+                    print "Serial responce: " ,serialConn.read_all()
+                else:
+                    print "No data in serial buffer"
